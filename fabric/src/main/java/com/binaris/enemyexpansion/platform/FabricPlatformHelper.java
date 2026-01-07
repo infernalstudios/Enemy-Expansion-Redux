4eceb999c0ce7422ca0fe4d2a7bc19cbe4529fca
Binary <binaris000@gmail.com>
2026-01-07 07:30:26 -0400

    :art: feat -> mod init!!

---
+++ fabric/src/main/java/com/binaris/enemyexpansion/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package com.binaris.enemyexpansion.platform;
+
+import com.binaris.enemyexpansion.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
---
+++ forge/src/main/java/com/binaris/enemyexpansion/platform/ForgePlatformHelper.java
@@ -0,0 +1,26 @@
+package com.binaris.enemyexpansion.platform;
+
+import com.binaris.enemyexpansion.platform.services.IPlatformHelper;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.FMLLoader;
+
+public class ForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+
+        return "Forge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return ModList.get().isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return !FMLLoader.isProduction();
+    }
+}
