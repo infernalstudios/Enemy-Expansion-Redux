da9a76c61da2361280aeab90dda170cc7d5ce770
evanbones <74276616+evanbones@users.noreply.github.com>
2026-01-23 01:04:18 -0800

    Sprinter armor

--- common/src/main/java/org/infernalstudios/enemyexpansion/client/entity/render/SprinterRenderer.java
+++ common/src/main/java/org/infernalstudios/enemyexpansion/client/entity/render/SprinterRenderer.java
@@ -1,18 +1,73 @@
 package org.infernalstudios.enemyexpansion.client.entity.render;
 
-import org.infernalstudios.enemyexpansion.client.entity.model.SprinterModel;
-import org.infernalstudios.enemyexpansion.content.entity.SprinterEntity;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ItemStack;
+import org.infernalstudios.enemyexpansion.client.entity.model.SprinterModel;
+import org.infernalstudios.enemyexpansion.content.entity.SprinterEntity;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib.cache.object.GeoBone;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.layer.ItemArmorGeoLayer;
 
 public class SprinterRenderer extends GeoEntityRenderer<SprinterEntity> {
     public SprinterRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new SprinterModel());
         this.shadowRadius = 0.5f;
+
+        addRenderLayer(new ItemArmorGeoLayer<>(this) {
+            @Nullable
+            @Override
+            protected ItemStack getArmorItemForBone(GeoBone bone, SprinterEntity animatable) {
+                return switch (bone.getName()) {
+                    case "armorhead" -> this.armorStackForBone(animatable, EquipmentSlot.HEAD);
+
+                    case "armorbody", "armorrightarm", "armorleftarm" ->
+                            this.armorStackForBone(animatable, EquipmentSlot.CHEST);
+
+                    case "armorpelvis", "armorrightleg", "armorleftleg" ->
+                            this.armorStackForBone(animatable, EquipmentSlot.LEGS);
+
+                    case "armorrightfoot", "armorleftfoot" -> this.armorStackForBone(animatable, EquipmentSlot.FEET);
+
+                    default -> null;
+                };
+            }
+
+            @Override
+            protected @NotNull EquipmentSlot getEquipmentSlotForBone(GeoBone bone, ItemStack stack, SprinterEntity animatable) {
+                return switch (bone.getName()) {
+                    case "armorhead" -> EquipmentSlot.HEAD;
+                    case "armorbody", "armorrightarm", "armorleftarm" -> EquipmentSlot.CHEST;
+                    case "armorpelvis", "armorrightleg", "armorleftleg" -> EquipmentSlot.LEGS;
+                    case "armorrightfoot", "armorleftfoot" -> EquipmentSlot.FEET;
+                    default -> super.getEquipmentSlotForBone(bone, stack, animatable);
+                };
+            }
+
+            @Override
+            protected @NotNull ModelPart getModelPartForBone(GeoBone bone, EquipmentSlot slot, ItemStack stack, SprinterEntity animatable, HumanoidModel<?> baseModel) {
+                return switch (bone.getName()) {
+                    case "armorhead" -> baseModel.head;
+                    case "armorbody", "armorpelvis" -> baseModel.body;
+                    case "armorrightarm" -> baseModel.rightArm;
+                    case "armorleftarm" -> baseModel.leftArm;
+                    case "armorrightleg", "armorrightfoot" -> baseModel.rightLeg;
+                    case "armorleftleg", "armorleftfoot" -> baseModel.leftLeg;
+                    default -> super.getModelPartForBone(bone, slot, stack, animatable, baseModel);
+                };
+            }
+
+            private ItemStack armorStackForBone(SprinterEntity entity, EquipmentSlot slot) {
+                return entity.getItemBySlot(slot);
+            }
+        });
     }
 
     @Override
